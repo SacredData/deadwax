7e7fb4769d59455ee59829516b722b1e20e3b6ad
Andrew Grathwohl <andrew@grathwohl.me>
2020-07-31 16:03:32 -0400

    Project and Recording WIP classes

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const Discogs = require('disconnect').Client
+const Side = require('./side')
+const { Album, Asset, Master, Release, Track, Vinyl } = require('baptism')
 
-const db = new Discogs().database()
+class Project {
+  constructor(opts={}) {
+    if (opts.discogsId) {
+      this.discogsId = opts.discogsId
+    }
+    if (opts.vinyl && opts.vinyl instanceof Vinyl) {
+      this.vinyl = opts.vinyl
+    }
+  }
+}
 
-db.getRelease(182213, function(err, data){
-    console.log(data.tracklist)
-})
+class Recording extends Project {
+  constructor(project, opts={}) {
+    super()
+    if (project && project instanceof Project) {
+      this.project = project
+    }
+  }
+}
+
+/*
+class Editing extends Recording {
+
+}
+
+class Mastering extends Editing {
+
+}
+*/
+
+module.exports = { Project, Recording }
